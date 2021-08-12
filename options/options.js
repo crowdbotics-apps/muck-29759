596f5c41e6841e42f0dad99a364e9e708f5ea44b
Crowdbotics <team@crowdbotics.com>
2021-08-12 14:18:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "muck_29759Identifier",
+  url: "https://muck_29759Identifier.botics.co",
+  api: "https://muck_29759Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
